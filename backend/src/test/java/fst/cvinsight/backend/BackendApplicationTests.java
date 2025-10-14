b976a06d0e9b517f33660c562bc4bac286aba475
IdrisLaabidi <idrislaabidi@gmail.com>
2025-10-14 19:48:01 +0100

    initial commit

---
+++ backend/src/test/java/fst/cvinsight/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fst.cvinsight.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
