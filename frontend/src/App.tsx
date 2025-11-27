c08eb45479843b691a68c23fcd05c0d965eb247e
IdrisLaabidi <idrislaabidi@gmail.com>
2025-11-27 16:38:52 +0100

    frontend: remove unnecessary pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,6 @@ import SignUp from "./pages/AuthPages/SignUp";
 import NotFound from "./pages/OtherPage/NotFound";
 import UserProfiles from "./pages/UserProfiles";
 import AccountSettings from "./pages/AccountSettingsPage/AccountSettings.tsx";
-import Videos from "./pages/UiElements/Videos";
-import Images from "./pages/UiElements/Images";
-import Alerts from "./pages/UiElements/Alerts";
-import Badges from "./pages/UiElements/Badges";
-import Avatars from "./pages/UiElements/Avatars";
-import Buttons from "./pages/UiElements/Buttons";
-import LineChart from "./pages/Charts/LineChart";
-import BarChart from "./pages/Charts/BarChart";
-import Calendar from "./pages/Calendar";
-import BasicTables from "./pages/Tables/BasicTables";
-import FormElements from "./pages/Forms/FormElements";
 import Blank from "./pages/Blank";
 import AppLayout from "./layout/AppLayout";
 import { ScrollToTop } from "./components/common/ScrollToTop";
@@ -68,7 +57,6 @@ export default function App() {
                                 {/* Others Page */}
                                 <Route path="/profile" element={<UserProfiles />} />
                                 <Route path="/account-settings" element={<AccountSettings />} />
-                                <Route path="/calendar" element={<Calendar />} />
                                 <Route path="/blank" element={<Blank />} />
 
                                 <Route path="/CvExtractionPage" element={<CvExtractionPage />}/>
@@ -77,24 +65,6 @@ export default function App() {
                                 <Route path="/resume-builder" element={<ResumeBuilder />} />
                                 <Route path="/resume-builder-v2" element={<CVBuilder />} />
 
-
-                                {/* Forms */}
-                                <Route path="/form-elements" element={<FormElements />} />
-
-                                {/* Tables */}
-                                <Route path="/basic-tables" element={<BasicTables />} />
-
-                                {/* Ui Elements */}
-                                <Route path="/alerts" element={<Alerts />} />
-                                <Route path="/avatars" element={<Avatars />} />
-                                <Route path="/badge" element={<Badges />} />
-                                <Route path="/buttons" element={<Buttons />} />
-                                <Route path="/images" element={<Images />} />
-                                <Route path="/videos" element={<Videos />} />
-
-                                {/* Charts */}
-                                <Route path="/line-chart" element={<LineChart />} />
-                                <Route path="/bar-chart" element={<BarChart />} />
                             </Route>
                         </Route>
 
